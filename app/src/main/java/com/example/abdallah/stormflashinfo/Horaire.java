b6f21972ae79835ffc1083ca9e5852c2a6cf1acb
abdsenouci <abdsenouci@gmail.com>
2018-05-11 00:14:04 +0200

    New launcher 4

--- app/src/main/java/com/example/abdallah/stormflashinfo/Horaire.java
+++ app/src/main/java/com/example/abdallah/stormflashinfo/Horaire.java
@@ -20,9 +20,9 @@ public class Horaire {
         Samedi = samedi;
         Dimanche = dimanche;
     }
-    /*
-    public static parseDay(){
 
+    public static String[] parseDay(String s){
+        return s.split(".");
     }
-    */
+
 }
