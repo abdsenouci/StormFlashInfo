f286278e6b72562b59ee5b770455e43632e996ac
Mourad <mouradlashab@yahoo.fr>
2018-05-10 19:33:36 +0200

    Ajout dateDeb2

--- app/src/main/java/com/example/abdallah/stormflashinfo/NouveauBonPlanActivity.java
+++ app/src/main/java/com/example/abdallah/stormflashinfo/NouveauBonPlanActivity.java
@@ -21,15 +21,17 @@ public class NouveauBonPlanActivity extends AppCompatActivity implements View.On
 {
     final String LOG = " NouveauBonPlanActivity";
 
+    static String FinalDateDeb;
+    static String FinalDateFin;
+
+    private DatePickerDialog.OnDateSetListener DateDebList;
+    private DatePickerDialog.OnDateSetListener DateFinList;
+    private EditText ObjBonPlan, DescBonPlan;
+
+    EditText DateDeb, DateFin;
+    Button btnInsert;
     String IdCategorie;
     String IdLieu;
-    static String FinalDateDeb;
-
-
-
-    private EditText ObjBonPlan, DescBonPlan, DateDeb, DateFin;
-    private DatePickerDialog.OnDateSetListener DateDebListner;
-    private Button btnInsert;
     int CategorieId = 0;
     int LieuId = 1;
 
@@ -52,19 +54,19 @@ public class NouveauBonPlanActivity extends AppCompatActivity implements View.On
             public void onClick(View view)
             {
                 Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
 
                 DatePickerDialog dialog = new DatePickerDialog
                         (NouveauBonPlanActivity.this, android.R.style.Theme_Holo_Dialog_MinWidth,
-                                DateDebListner, year, month, day);
+                                DateDebList, year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
 
         });
-        DateDebListner = new DatePickerDialog.OnDateSetListener()
+        DateDebList = new DatePickerDialog.OnDateSetListener()
         {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day)
@@ -79,6 +81,36 @@ public class NouveauBonPlanActivity extends AppCompatActivity implements View.On
 
 
         DateFin = findViewById(R.id.DateFin);
+        DateFin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Calendar cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+
+                DatePickerDialog dialog = new DatePickerDialog
+                        (NouveauBonPlanActivity.this, android.R.style.Theme_Holo_Dialog_MinWidth,
+                                DateFinList, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+
+        });
+        DateFinList = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String A = String.valueOf(year);
+                String M = String.valueOf(month);
+                String J = String.valueOf(day);
+                FinalDateFin = A + "/" + M + "/" + J;
+            }
+        };
         btnInsert = (Button)findViewById(R.id.btnInsert);
         btnInsert.setOnClickListener(this);
 
@@ -88,17 +120,14 @@ public class NouveauBonPlanActivity extends AppCompatActivity implements View.On
     }
 
 
-
     @Override
     public void onClick(View v)
     {
         HashMap postData = new HashMap();
         postData.put("TxtObjBonPlan", ObjBonPlan.getText().toString());
         postData.put("TxtDescBonPlan", DescBonPlan.getText().toString());
-        //postData.put("TxtDateDeb", DateDeb.getText().toString());
         postData.put("TxtDateDeb", FinalDateDeb);
-        postData.put("TxtDateFin", DateFin.getText().toString());
-        //postData.put("TxtDateFin", "30/11/2018");
+        postData.put("TxtDateFin", FinalDateFin);
         postData.put("TxtIdCat", IdCategorie);
         postData.put("TxtIdLieu", IdLieu);
         //postData.put("Mobile", "Android");
@@ -116,6 +145,8 @@ public class NouveauBonPlanActivity extends AppCompatActivity implements View.On
                     Intent in = new Intent(NouveauBonPlanActivity.this, ListeBonPlansActivity.class);
                     startActivity(in);
                 }
+                else
+                    finish();
             }
         });
         taskInsert.execute("http://10.0.2.2:8888/StormFlash/AjoutBonPlan2.php");
