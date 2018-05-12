cc43c897304ba19f4a1b51078f3b8bb3dda2ded3
abdsenouci <abdsenouci@gmail.com>
2018-05-12 04:22:00 +0200

    Horaires v2

--- app/src/main/java/com/example/abdallah/stormflashinfo/Horaire.java
+++ app/src/main/java/com/example/abdallah/stormflashinfo/Horaire.java
@@ -2,25 +2,18 @@ package com.example.abdallah.stormflashinfo;
 
 public class Horaire {
     public int IdHoraire;
-    public String Lundi;
-    public String Mardi;
-    public String Mercredi;
-    public String Jeudi;
-    public String Vendredi;
-    public String Samedi;
-    public String Dimanche;
     public int[][] planning;
 
     public Horaire(int idHoraire, String lundi, String mardi, String mercredi, String jeudi, String vendredi, String samedi, String dimanche) {
         IdHoraire = idHoraire;
-        Lundi = lundi;
-        Mardi = mardi;
-        Mercredi = mercredi;
-        Jeudi = jeudi;
-        Vendredi = vendredi;
-        Samedi = samedi;
-        Dimanche = dimanche;
         planning = new int[7][3];
+        this.setJour(lundi,0);
+        this.setJour(mardi, 1);
+        this.setJour(mercredi, 2);
+        this.setJour(jeudi, 3);
+        this.setJour(vendredi, 4);
+        this.setJour(samedi, 5);
+        this.setJour(dimanche, 6);
     }
 
     public static int getPositionHoraire(int idHoraire)
@@ -34,17 +27,6 @@ public class Horaire {
             }
         }
         return position;
-    }
-
-    public void parseAll()
-    {
-        this.setJour(this.Lundi,0);
-        this.setJour(this.Mardi, 1);
-        this.setJour(this.Mercredi, 2);
-        this.setJour(this.Jeudi, 3);
-        this.setJour(this.Vendredi, 4);
-        this.setJour(this.Samedi, 5);
-        this.setJour(this.Dimanche, 6);
     }
 
     private void setJour(String jour, int id)
