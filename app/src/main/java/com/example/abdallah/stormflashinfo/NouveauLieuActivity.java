d9099cc4899f764880b815937e9d80c241e3d2ce
Mourad <mouradlashab@yahoo.fr>
2018-05-12 19:11:07 +0200

    ya R

--- app/src/main/java/com/example/abdallah/stormflashinfo/NouveauLieuActivity.java
+++ app/src/main/java/com/example/abdallah/stormflashinfo/NouveauLieuActivity.java
@@ -2,15 +2,24 @@ package com.example.abdallah.stormflashinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
-public class NouveauLieuActivity extends AppCompatActivity {
+public class NouveauLieuActivity extends AppCompatActivity implements View.OnClickListener
+{
+    final String LOG = " NouveauLieuActivity";
+
+    EditText NomLieu, AdrLieu, CpLieu, TelLieu,
+            Lu1, Lu2, Ma1,Ma2, Me1, Me2, Je1, Je2, Ve1, Ve2, Sa1,Sa2, Di1, Di2;
+    Switch Lun, Mar, Mer, Jeu, Ven, Sam;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nouveau_lieu);
-
-
     }
 }
