14a2ac5e065339b2ceca9472f5aca4d472c0db93
abdsenouci <abdsenouci@gmail.com>
2018-05-12 04:10:04 +0200

    Horaires

--- app/src/main/java/com/example/abdallah/stormflashinfo/Horaire.java
+++ app/src/main/java/com/example/abdallah/stormflashinfo/Horaire.java
@@ -9,6 +9,7 @@ public class Horaire {
     public String Vendredi;
     public String Samedi;
     public String Dimanche;
+    public int[][] planning;
 
     public Horaire(int idHoraire, String lundi, String mardi, String mercredi, String jeudi, String vendredi, String samedi, String dimanche) {
         IdHoraire = idHoraire;
@@ -19,6 +20,7 @@ public class Horaire {
         Vendredi = vendredi;
         Samedi = samedi;
         Dimanche = dimanche;
+        planning = new int[7][3];
     }
 
     public static int getPositionHoraire(int idHoraire)
@@ -34,10 +36,23 @@ public class Horaire {
         return position;
     }
 
-    public int[][] parseJours()
+    public void parseAll()
     {
+        this.setJour(this.Lundi,0);
+        this.setJour(this.Mardi, 1);
+        this.setJour(this.Mercredi, 2);
+        this.setJour(this.Jeudi, 3);
+        this.setJour(this.Vendredi, 4);
+        this.setJour(this.Samedi, 5);
+        this.setJour(this.Dimanche, 6);
+    }
 
-        int[][] res = {{0,1,2,3,4,5,6}{this.Lundi, this.Mardi, this.Mercredi, this.}};
+    private void setJour(String jour, int id)
+    {
+        String[] parsed = jour.split(".");
+        planning[id][0]= Integer.valueOf(parsed[0]);
+        planning[id][1]= Integer.valueOf(parsed[1]);
+        planning[id][2]= Integer.valueOf(parsed[2]);
     }
 
 }
